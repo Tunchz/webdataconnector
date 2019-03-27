3fe838f5fc3ef42d407a383b6fd8940c739ab792
Tunchz <tunchz@gmail.com>
2019-03-27 11:45:05 +0700

    Update ElasticsearchSensor.js

--- Examples/js/ElasticsearchSensor.js
+++ Examples/js/ElasticsearchSensor.js
@@ -16,8 +16,7 @@
             id: "Light",
             //alias: "unit",
             dataType: tableau.dataTypeEnum.float
-        }, 
-{
+        }, {
             id: "Soil",
             //alias: "latitude",
             dataType: tableau.dataTypeEnum.int
@@ -56,7 +55,7 @@
     // Download the data
     myConnector.getData = function(table, doneCallback) {
         //$.getJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson", function(resp) {
-        $.getJSON("http://122.155.11.34:9200/mysensor/_search?q=*:*&sort=timestamp:desc&size=100", function(resp) {
+        $.getJSON("http://122.155.11.34:9200/mysensor/_search?q=*:*&sort=timestamp:desc&size=60", function(resp) {
             var feat = resp.hits.hits,
                 tableData = [];
 
