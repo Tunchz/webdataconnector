3a999377212848fb3118e2e96030a758773b91bf
Tunchz <tunchz@gmail.com>
2019-03-28 10:35:28 +0700

    try D3 to get json

--- Examples/js/ElasticsearchSensor.js
+++ Examples/js/ElasticsearchSensor.js
@@ -56,7 +56,8 @@
     // Download the data
     myConnector.getData = function(table, doneCallback) {
         //$.getJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson", function(resp) {
-        $.getJSON("http://122.155.11.34:9200/mysensor/_search?q=*:*&sort=timestamp:desc&size=60", function(resp) {
+        //$.getJSON("http://122.155.11.34:9200/mysensor/_search?q=*:*&sort=timestamp:desc&size=60", function(resp) {
+    	d3.json("http://122.155.11.34:9200/mysensor/_search?q=*:*&sort=timestamp:desc&size=60", function(resp) {
             var feat = resp.hits.hits,
                 tableData = [];
 
